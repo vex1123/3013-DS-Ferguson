fac3e6357728f92a36fdd8b89764ff2573834dcf
vex1123 <55103161+vex1123@users.noreply.github.com>
2019-10-11 13:02:58 -0500

    Update priority_heap.cpp

--- assignments/A03/priority_heap.cpp
+++ assignments/A03/priority_heap.cpp
@@ -1,9 +1,16 @@
-//Ryan Ferguson
-//3013 - MW 3 - 4:20
-// -----------------
-// This program creates a binary max heap that organises an object file based on a calculated priority
-// This file uses json for c++
-// This program is not finished and will not properly run, it reaches a read acess vilation when inserting an object.
+/**
+ * Name: Ryan Ferguson
+ * Date: 10/11/2019
+ * Course: CMPS 3013 MW 3- 4:20
+ * Program: A03
+ * Description:
+ *
+ *	   This program reads data from a .json file, and uses it to create
+ *	   an array of Animal pointers. It initializes a heap full of animal
+ *	   arrays as a size of 1000. Then the program calls a funtion to insert
+ *	   the Animal information from the .json file into the heap. It then
+ *	   extracts the top 5 prioty animals form each created heap
+ */
 
 
 #include "json_helper.cpp"
@@ -113,7 +120,7 @@ public:
 			obj = J->getNext();
 			Animals[i] = new Animal(obj);
 			Animals[i]->priority = (6372.8 - Animals[i]->date) *Animals[i]->adjuster / Animals[i]->latitude;
-			if(Animals[i]->validated == true)
+			if (Animals[i]->validated == true)
 			{
 				Animals[i]->priority *= -1;
 			}
@@ -182,21 +189,20 @@ using namespace std::chrono;
 */
 class Heap {
 private:
-	Animal *H;
 	Animal **A;       // Pointer to allocate dynamic array
 	int Next;     // Next available location
 	int MaxSize;  // Max size since were using array
 	int HeapSize; // Actual number of items in the array.
 	int size;
-				  /**
-				  * Function IncreaseKey:
-				  *      Bubbles element up from given index.
-				  *
-				  * Params:
-				  *     [int] index  - index of item to be increased
-				  * Returns
-				  *      void
-				  */
+	/**
+	* Function IncreaseKey:
+	*      Bubbles element up from given index.
+	*
+	* Params:
+	*     [int] index  - index of item to be increased
+	* Returns
+	*      void
+	*/
 	void BubbleUp(int i) {
 		int p = Parent(i);
 		while (p > 0 && A[i]->priority > A[p]->priority) {
@@ -318,15 +324,15 @@ public:
 	/**
 	* Function Heap:
 	*      Constructor that allocates memory for array and
-	*      inits vars.
+	*      inits vars. Creates Animal array of int size
 	*
 	* Params:
-	*      void
+	*      int size
 	* Returns
 	*      void
 	*/
 	Heap(int size) {
-		H = new Animal[size];
+		A = new Animal*[size];
 		Next = 1;
 		MaxSize = size;
 		HeapSize = 0;
@@ -334,14 +340,14 @@ public:
 
 	/**
 	* Function Insert:
-	*      Insert value into heap.
+	*      Insert Animal into heap.
 	*
 	* Params:
-	*      [int] x - value to be inserted
+	*        Animal *
 	* Returns
 	*      void
 	*/
-	void Insert(Animal* x) {
+	void Insert(Animal *x) {
 		A[Next] = x;
 		BubbleUp(Next);
 		Next++;
@@ -355,22 +361,18 @@ public:
 
 	/**
 	* Function Extract:
-	*      Removes top element from heap (whether min or max).
-	*
+	*      Removes top Animal priority from heap (whether min or max).
+	*	   
 	* Params:
 	*      void
 	* Returns
 	*      [int] top_value - top value in the heap (min or max)
 	*/
-	Animal Extract(Animal x) {
+	Animal Extract() {
 
-		if (Empty()) {
-			return -1;
-		}
 
-		int retval = A[1]->priority;
-		string retname = A[1]->animal_name;
-		H[1] = H[--Next];
+		Animal retval = *A[1];
+		A[1] = A[--Next];
 		HeapSize--;
 
 		if (HeapSize > 1) {
@@ -425,14 +427,15 @@ public:
 
 	/**
 	* Function Heapify:
-	*      Creates a heap out of a given array of nums in no specific order.
+	*      Creates a heap out of a given array of animals in no specific order.
 	*
 	* Params:
-	*      [int*] array - array of values to heapify
-	*      [int] size - size of array
+	*      Animal *array
+	*	   int size
 	* Returns
 	*      void
 	*/
+
 	void Heapify(Animal *A, int size) {
 		int i = size / 2;
 		// H = A;
@@ -445,37 +448,81 @@ public:
 	}
 };
 
+/**
+	* Function top5:
+	*      extracts the top 5 animals in order of priority
+	*	   and sends thier values to an outfile
+	* Params:
+	*      Heap **&, Animal **&, int, ofstream&  
+	* Returns
+	*      void
+	*/
+void top5(Heap **&Buckets, Animal **&Animals, int NumBuckets, ofstream &outfile) {
+	Animal temp;
+	outfile << "Ryan Ferguson" << endl;
+	for (int i = 0; i < NumBuckets; i++) {
+		outfile << "Heap # " << i + 1 << endl;
+		outfile << "-------------" << endl;
+		for (int j = 0; j < 5; j++) {
+			temp = Buckets[i]->Extract();
+			outfile << temp.animal_name << " " << temp.priority << endl;
+		}
+		outfile << endl;
+	}
+}
+
+/**
+	* fillBuckets:
+	*      Initializes each bucket with the animal array
+	*
+	* Params:
+	*      Heap **&, Animal**&, int size, int numBuckets
+	* Returns
+	*      void
+	*/
+void fillBuckets(Heap **&Buckets, Animal **&Animals, int size, int NumBuckets) {
+	int which;
+	for (int i = 0; i < size; i++) {
+		which = abs(Animals[i]->date) % NumBuckets;
+		Buckets[which]->Insert(Animals[i]);
+	}
+}
+
 
 int main(int argc, char **argv) {
-	int size = 3;
-	/*
-	if (argc < 2) {
-		cout << "Error: need parameter!";
-		char ch;
-		cin.get(ch);
-		exit(0);
-	}else {
-		size = (int)argv[1];
-		cout << "Buckets: " << size << endl;
-	}
-	*/
-		
-	AnimalHelper AH("animals.json");
+	//int size = 3;
+	//debugging code for non variable testing
+	ofstream outfile;
+	outfile.open("out.txt");
 	
+
+	//accept variable of 3- 11 and create that ubmer of buckets
+	int size;
+	if (argc > 1) {
+		size = atoi(argv[1]);
+	}
+	else {
+		cout << "ERROR: no integer 3 - 11 entered" << endl;
+		exit(0);
+	}
+	
+	AnimalHelper AH("animals.json");
+
+	int which;
+	int total = 1000;
 	Heap **Buckets;
 	Buckets = new Heap*[size];
-	Animal ** animals = AH.getAnimalPointer(AH);  
-	for (int i = 0; i< size; i++) {
+	Animal ** animals = AH.getAnimalPointer(AH);
+	
+	for (int i = 0; i < size; i++) {
 		Buckets[i] = new Heap(1000);
 	}
 
-	for(int i = 0; i < size; i++)
-	{
-		Buckets[i]->Insert(animals[i]);
-	}
+	//fill the buckets
+	fillBuckets(Buckets, animals, total, size);
 	
-
-	AH.PrintAnimals();
+	//extract top 5 prioritys
+	top5(Buckets, animals, size, outfile);
 
 	return 0;
 }
